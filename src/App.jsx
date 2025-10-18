91bf9794654a46520af3645a11a54fd08491212c
berkvakar <berkvakar1@gmail.com>
2025-10-18 16:24:42 -0700

    Completed landing page layout + styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import { useState, Row, Col } from 'react'
-import './App.css'
+// import { useState } from 'react'
+import { Row, Col, Container } from "react-bootstrap";
+import Logos from "./assets";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Row>
-        <Col>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </Col>
-      </Row>
+      <Container fluid className="container">
+        <Row className="header">
+          <Col className="header-col">
+            <button>Articles</button>
+          </Col>
+          <Col>
+            <a href="/">
+              <img src={Logos.LogoWhite} alt="AF Logo" className="af-logo" />
+            </a>
+          </Col>
+          <Col className="header-col">
+            <button>AF Partnership Program</button>
+          </Col>
+        </Row>
+        <Row className="main-body">
+          <Col>
+            <h1 className="main-message">Athletic Finance simplifies business x sports.</h1>
+            <button className= "join-button">Join</button>
+          </Col>
+          
+        </Row>
+        <Row className="footer-row">
+          <Col xs="auto">
+             <button className="footer-button" >Enquiries</button>
+          </Col>
+          <Col xs="auto">
+             <button className="footer-button">Legal</button>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import LogoWhite from  "./LogoWhite.png";
+import LogoBlack from "./LogoBlack.png";
+import LogoWhiteXL from "./LogoWhiteXL.png";
+import LogoBlackXL from "./LogoBlackXL.png";
+
+export default {
+    LogoWhite,
+    LogoBlack,
+    LogoWhiteXL,
+    LogoBlackXL
+}
